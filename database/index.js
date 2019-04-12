0d081a0c34be539ee27f93c3d5d862bf3c598317
RamKhalsa <khalsa.ramdas@gmail.com>
2019-04-11 19:56:26 -0700

    Implement Docker DB Auto-connection

--- database/index.js
+++ database/index.js
@@ -1,15 +1,28 @@
 const Sequelize = require('sequelize');
-const pw = process.env.MYSQL_ROOT_PW || require('./config/sequelize.config').rootPW;
+const mysql = require('mysql2/promise');
+const pw = process.env.MYSQL_ROOT_PASSWORD || require('./config/sequelize.config').rootPW;
 
 const databaseName = process.env.MYSQL_DATABASE || 'bookings';
 
+mysql.createConnection({
+  host: process.env.MYSQL_HOST || '172.17.0.2',
+  port: process.env.MYSQL_PORT || '3306',
+  user: process.env.MYSQL_USER || 'root',
+  password: pw || 'root',
+})
+  .then((connection) => {
+    connection.query(`CREATE DATABASE IF NOT EXISTS ${databaseName};`).then(() => {
+      console.info('Database create or successfully checked');
+      connection.end();
+    });
+});
 
 const db = new Sequelize(databaseName, 'root', pw, {
-  host: process.env.MYSQL_URL || 'localhost',
+  host: process.env.MYSQL_URL || '172.17.0.2',
   dialect: 'mysql',
   logging: false,
 });
 
-db.query('CREATE DATABASE IF NOT EXISTS bookings').then(() => console.log('Database created'));
+// db.query(`CREATE DATABASE IF NOT EXISTS ${databaseName};`).then(() => console.info('Database created'));
 
 module.exports = db;
